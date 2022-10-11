a9a4ba35d1b5cd80093b99c94ea7ab9545014f52
Bartcus <marius.bartcus@gmail.com>
2022-10-11 11:40:38 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# oc_7_app
+This is an application of sentiment analysis for the Open Classroom 7th project in AI training. I use this repository to deploy it with heroku. 
---
+++ web.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import requests
+
+
+
+
+st.title('Sentiment analyse')
+st.subheader('Air Paradis')
+
+with st.form(key='tweet_form', clear_on_submit=True):
+    my_tweet = st.text_input('Tweet something here:')
+
+    submit_button = st.form_submit_button('Submit')
+
+if submit_button:
+    with st.spinner('Wait for it...'):
+        st.info(f'Your tweet is :  {my_tweet}')
+
+        result = requests.get("https://sentimentanalyseapi.herokuapp.com/api", params={"my_tweet": my_tweet}).json()
+
+        sentiment = int(result['sentiment'])
+        prob = float(result['prob'])
+
+        if sentiment==1:
+            st.success('This is a {:.2f}% positive tweet :thumbsup:'.format(prob*100))
+        else:
+            st.error('This is a {:.2f}% negative tweet :thumbsdown:'.format(prob*100))
